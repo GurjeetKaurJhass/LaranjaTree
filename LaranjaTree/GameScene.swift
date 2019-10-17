0213e9f69be1cca9e9319c2e32a7ee1428799492
gurjeet <gurjit.babrah@gmail.com>
2019-10-16 21:23:14 -0400

    setup collision rules for ground and orange

--- LaranjaTree/GameScene.swift
+++ LaranjaTree/GameScene.swift
@@ -2,7 +2,9 @@
 //  GameScene.swift
 //  LaranjaTree
 //
-//  Created by Gurjeet kaur on 2019-10-11.
+//  Created by Gurjeet kaur o self.ground = self.childNode(withName: "ground") as! SKSpriteNode
+
+      
 //  Copyright © 2019 The Lambton. All rights reserved.
 //
 
@@ -10,9 +12,19 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene,SKPhysicsContactDelegate {
-    
+    var ground:SKSpriteNode!
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        self.ground = self.childNode(withName: "ground") as! SKSpriteNode
+        
+              self.ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+               self.ground.physicsBody?.affectedByGravity = false
+               self.ground.physicsBody?.categoryBitMask = 32
+               self.ground.physicsBody?.collisionBitMask = 0
+               self.ground.physicsBody?.contactTestBitMask = 0
+        
+        
+        
         }
     override func update(_ currentTime: TimeInterval) {
     
@@ -65,6 +77,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
            orange.position.y = mouseYPosition
             // 3. show the orange on screen
            addChild(orange)
+           orange.zPosition = 999
         
         // ADD PHYSICS TO THE ORANGE
                // ---------------------------
@@ -72,6 +85,14 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
                  orange.physicsBody = SKPhysicsBody(circleOfRadius: orange.size.width / 2)
                // 2. Set gravity / dynamic for the orange
                 orange.physicsBody?.affectedByGravity = false
+         // 2. Give orange gravity. By default, orange is affected by gravity.
+                // orange.physicsBody?.affectedByGravity = true
+               // 3. Set category, collision, and contact bit masks
+               //  - By default, collison = everything, contact = 0
+                orange.physicsBody?.categoryBitMask = 1
+               orange.physicsBody?.contactTestBitMask = 0
+        
+        
          }
     
    
